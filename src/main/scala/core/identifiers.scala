384132c1bbee9ebce005f6094f45db0352554ffb
medulla <medulla@cpmacbook.local>
2017-04-10 12:48:51 -0400

    see previous commit message

--- src/main/scala/core/identifiers.scala
+++ src/main/scala/core/identifiers.scala
@@ -7,128 +7,127 @@ final class IdDecimal(val bd : BigDecimal, override val mc : MathContext) extend
 }
 */
 
-/* This is likely a bad way to do this. This is my first scala file I've ever written, so back off!
-TODO: figure out the scala way to do this
- */
-object IdUtils{
-  def truncatedBigDecimal(bd: BigDecimal) : BigDecimal = {
+object IdUtils {
+  def truncatedBigDecimal(bd: BigDecimal): BigDecimal = {
     BigDecimal(bd.underlying.stripTrailingZeros())
   }
 }
 
-trait Id{
-  def getId(): String
+trait Id {
+  val id : String
+  override def toString: String = this.id
+  override def equals(that: Any): Boolean = {
+    this.getClass == that.getClass && this.id == that.id
+  }
+  override def hashCode(): Int = this.id.hashCode
+}
 
-  override def toString: String = getId()
+// Use if String Id becomes more complicated
+// trait StringId(val id: String)
 
-  override def equals(that : Any)  : Boolean = {
+class TimeSeriesId(val id: String) extends Id {}
+
+class SubjectId(val id: String) extends Id {}
+
+class ExperimentId(val id: String) extends Id {}
+
+class BlockId(val id: String) extends Id {}
+
+// Preprocessing Readable ID
+class ProcessSlugId(val id: String) extends Id {}
+
+class StimuliSetId(val id: String) extends Id {}
+
+class Stimulus(val id: String) {}
+
+class RecordingChannelId(val id: String) extends Id {}
+
+class DataId(val id: String) extends Id {}
+
+// timeString is string of epoch
+class Timestamp(val timeString: String) extends Ordered[Timestamp] {
+  private val timeDecimal: BigDecimal =
+    IdUtils.truncatedBigDecimal(BigDecimal(timestamp))
+  val _underlyingDB: BigDecimal = t
+
+  override def equals(that: Any): Boolean = {
     this.getClass == that.getClass && this.toString == that.toString
   }
+  override def hashCode(): Int = this.timeString.hashCode
+  override def toString: String = this.timeString
 
-  override def hashCode(): Int = this.toString.hashCode
-}
-
-class TimeSeriesId(timeseriesId : String) extends Id{
-  def getId() = timeseriesId
-}
-
-class SubjectId(subjectId : String) extends Id{
-  def getId() = subjectId
-}
-
-class ExperimentId(experimentId : String) extends Id{
-  def getId() = experimentId
-}
-
-class BlockId(blockId : String) extends Id{
-  def getId() = blockId
-}
-
-class ProcessSlugId(processSlugId : String) extends Id{
-  def getId() = processSlugId
-}
-
-class StimuliSetId(stimuliSetId : String) extends Id{
-  def getId() = stimuliSetId
-}
-
-class Stimulus(stimulus : String) {
-  def getStimulus() = stimulus
-}
-
-class RecordingChannelId(channelId : String) extends Id{
-  def getId = channelId
-}
-
-class Timestamp(timestamp : String) extends Ordered[Timestamp]{
-  private val t : BigDecimal = IdUtils.truncatedBigDecimal(BigDecimal(timestamp))
-  val _underlyingDB : BigDecimal = t
-
-  override def equals(that : Any)  : Boolean = {
-    this.getClass == that.getClass && this.toString == that.toString
+  override def compare(that: Timestamp) = {
+    this.timeDecimal.compare(that.timeDecimal)
   }
-  override def hashCode(): Int = this.toString.hashCode
-  override def toString: String = t.toString
-
-  override def compare(that: Timestamp) = this.t.compare(that.t)
 }
 
-object Timestamp {
-  def apply(timestamp : String) = new Timestamp(timestamp)
+object Timestamp { 
+  def apply(timeString: String) = new Timestamp(timeString)
 }
 
-class Event(timestamp : Timestamp) {
-  val t = timestamp
+class Event(val timestamp: Timestamp) {}
+
+class EventVector(events: IndexedSeq[Event])
+
+class TriggerEvent(val triggerValue: Int, val timestamp: Timestamp)
+  extends Event(timestamp)
+
+class TriggerEventVector(events: Vector[TriggerEvent])
+
+class StimulusEvent(stimulus: Stimulus, timestamp: Timestamp)
+  extends Event(timestamp)
+{}
+
+class StimulusEventVector(val stimuliEvents: IndexedSeq[StimulusEvent]) {}
+
+class RecordingId(val id: String) extends Id {}
+  
+  
+  // How do traits extend when there are constructors
+  // ESB - Experimental Subject block.
+trait ESBRecordingId extends RecordingId(id) {
+  val subjectId : ExperimentId
+  val experimentId : ExperimentID
+  val blockId : BlockId
+  val subjectId : SubjectId
+  val experimentId : ExperimentId
 }
-
-class EventVector(events : IndexedSeq[Event])
-
-class TriggerEvent(triggerValue : Int, timestamp : Timestamp) extends Event(timestamp)
-class TriggerEventVector(events : Vector[TriggerEvent])
-
-class StimulusEvent(stimulus : Stimulus, timestamp : Timestamp) extends Event(timestamp){
-}
-
-class StimulusEventVector(stimuliEvents : IndexedSeq[StimulusEvent]){
-  def getStimuliEvents() = stimuliEvents
-
-}
-
-class DataId(dataId : String) extends Id{
-  def getId() = dataId
-}
-
-class RecordingId(recordingId : String) extends Id {
-  override def getId() = recordingId
-}
-
-trait ESBRecordingId extends RecordingId {
-  def getSubject() : SubjectId
-  def getExperiment() : ExperimentId
-  def getBlock() : BlockId
-}
-
-class AlphaESBRecordingId(recordingId : String) extends RecordingId(recordingId)
-  with ESBRecordingId{
+/
+// / id - string seperated
+class SimpleESBRecordingId(val id: String)
+  extends ESBRecordingId
+{
   val separatorString = "_"
-  val components = recordingId.split(separatorString)
-  val experiment : String = components(0)
-  val subject : String = components(1)
-  val blockId : String = components(2)
-  def getSubject() : SubjectId = new SubjectId(subject)
-  def getExperiment() : ExperimentId =  new ExperimentId(experiment)
-  def getBlock() : BlockId = new BlockId(blockId)
+  val components = id.split(separatorString)
+  val experiment: String = components(0)
+  val subject: String = components(1)
+  val blockId: String = components(2)
+  def getSubject(): SubjectId = new SubjectId(subject)
+  def getExperiment(): ExperimentId = new ExperimentId(experiment)
+  def getBlock(): BlockId = new BlockId(blockId)
 }
 
-class UEL(recordingId : RecordingId, timestamp : Timestamp) extends Id {
+
+class UEL(recordingId: RecordingId, val timestamp: Timestamp) extends Id {
   val separatorString = ":"
-  val t = timestamp
-  def getId() = Array(recordingId, t.toString()).mkString(separatorString)
+  def getId() = Array(
+    recordingId,
+    timestamp.toString()).mkString(separatorString
+  )
 }
 
-class UNL(val recordingId: RecordingId, val startTime : Timestamp, val endTime : Timestamp) extends Id{
+class UNL(
+           val recordingId: RecordingId,
+           val startTime: Timestamp,
+           val endTime: Timestamp)
+  extends Id
+{
   val separatorString = "-"
-  val identifier = Array(startTime.toString(), endTime.toString()).mkString(separatorString)
+  val identifier =
+    Array(
+      startTime.toString(),
+      endTime.toString()).mkString(separatorString
+    )
   def getId() = identifier
 }
 
